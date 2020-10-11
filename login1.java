213a0605f1995f81ba3508104a3cde83b1349f56
nihara75 <niharar25@gmail.com>
2020-10-11 13:17:53 +0530

    first commit

---
+++ login1.java
@@ -0,0 +1,282 @@
+
+
+import java.sql.*;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+
+public class login1 extends javax.swing.JFrame {
+
+    public login1() {
+        initComponents();
+        
+        
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jColorChooser1 = new javax.swing.JColorChooser();
+        jColorChooser3 = new javax.swing.JColorChooser();
+        jPanel1 = new javax.swing.JPanel();
+        jColorChooser2 = new javax.swing.JColorChooser();
+        jPanel2 = new javax.swing.JPanel();
+        label2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(25, 16, 7));
+        jPanel1.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
+
+        jPanel2.setBackground(new java.awt.Color(35, 35, 116));
+
+        label2.setIcon(new javax.swing.ImageIcon("/home/nihara/Downloads/index.png")); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(274, 274, 274)
+                .addComponent(label2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(29, Short.MAX_VALUE))
+        );
+
+        jLabel1.setFont(new java.awt.Font("Garuda", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(239, 239, 244));
+        jLabel1.setText("User name");
+
+        jLabel2.setBackground(new java.awt.Color(239, 239, 244));
+        jLabel2.setFont(new java.awt.Font("Garuda", 0, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(251, 246, 246));
+        jLabel2.setText("Password");
+
+        jButton1.setBackground(new java.awt.Color(83, 83, 54));
+        jButton1.setFont(new java.awt.Font("Noto Sans CJK TC Regular", 0, 24)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(224, 210, 186));
+        jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(131, 131, 131)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(276, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jColorChooser2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(256, Short.MAX_VALUE)))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(52, 52, 52)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(71, 71, 71)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(71, 71, 71)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(111, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addGap(0, 653, Short.MAX_VALUE)
+                    .addComponent(jColorChooser2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //System.out.print("connect");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/javapro","nihu","Nihar@25*");
+        Statement st=con.createStatement();
+        String query="Select password from Faculty where username='"+jTextField1.getText()+"'";
+        ResultSet rs=st.executeQuery(query);
+        String get_passwords="";
+        while(rs.next()){
+       get_passwords=rs.getString(1);
+        
+        }
+        if(get_passwords.equals(String.valueOf(jPasswordField1.getPassword())))
+        {if(jTextField1.getText().equalsIgnoreCase("COMPUTER"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept1 a=new dept1();
+        a.setVisible(true);
+        
+              
+        }
+        
+        
+        if(jTextField1.getText().equalsIgnoreCase("INDUSTRIAL"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept2 a=new dept2();
+        a.setVisible(true);
+        
+              
+        }
+        
+        if(jTextField1.getText().equalsIgnoreCase("MECHANICAL"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept3 a=new dept3();
+        a.setVisible(true);
+        
+              
+        }
+        if(jTextField1.getText().equalsIgnoreCase("ELECTRONICS"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept4 a=new dept4();
+        a.setVisible(true);
+        
+              
+        }
+        if(jTextField1.getText().equalsIgnoreCase("CIVIL"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept5 a=new dept5();
+        a.setVisible(true);
+        
+              
+        }
+        if(jTextField1.getText().equalsIgnoreCase("APELECTRONICS"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept6 a=new dept6();
+        a.setVisible(true);
+        
+              
+        }
+        if(jTextField1.getText().equalsIgnoreCase("ELECTRICAL"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        dept7 a=new dept7();
+        a.setVisible(true);
+        
+              
+        }
+        if(jTextField1.getText().equalsIgnoreCase("TVECET"))
+        {JOptionPane.showMessageDialog(null,"LOGIN SUCCESSFUL");
+       
+        PRIN a=new PRIN();
+        a.setVisible(true);
+        
+              
+        }
+        }
+        else
+        {
+        JOptionPane.showMessageDialog(null, "UNSUCCESSFUL");
+        
+        }
+        
+        }
+        catch(Exception e)
+        {
+        System.out.print(e);}
+    
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(login1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new login1().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JColorChooser jColorChooser1;
+    private javax.swing.JColorChooser jColorChooser2;
+    private javax.swing.JColorChooser jColorChooser3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel label2;
+    // End of variables declaration//GEN-END:variables
+}
